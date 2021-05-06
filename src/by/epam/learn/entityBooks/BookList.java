f0dd54aa1772f7d50027554e675d27acc4c5a895
Svetlaja <72805375+Svetlaja@users.noreply.github.com>
2021-05-07 00:09:22 +0300

    Delete src directory

--- src/by/epam/learn/entityBooks/BookList.java
+++
@@ -1,8 +0,0 @@
-package by.epam.learn.entityBooks;
-
-public class BookList extends BookArray {
-    public static void main(String[] args) {
-        BookList list = new BookList();
-        list.BookArray();
-    }
-}
