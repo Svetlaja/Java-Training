96c231bfa3980f12ad7ffef73d1fc912f1764cfa
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-07-05 10:33:48 +0300

    Delete src/by/epam/learn directory

--- src/by/epam/learn/collectionsCD/AudioCDMain.java
+++
@@ -1,9 +0,0 @@
-package by.epam.learn.collectionsCD;
-
-    public class AudioCDMain extends AudioCD  {
-        public static void main (String[] args){
-
-            AudioCDMain obj = new AudioCDMain();
-            obj.AudioCD();
-        }
-    }
--- src/by/epam/learn/entityBooks/BookListMain.java
+++
@@ -1,9 +0,0 @@
-package by.epam.learn.entityBooks;
-
-public class BookListMain extends BookArray {
-
-    public static void main(String[] args) {
-        BookListMain list = new BookListMain();
-        list.BookArray();
-    }
-}
