4068f0a9fc456a5005a0315879420ca93cda11af
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-10-21 13:06:14 +0300

    Updated the Main Task from modul Java.Threads - package package by.epam.learn.Airport
    Developed console multithreaded application Airport using java.util.concurrent.

---
+++ src/by/epam/learn/Airport/AircraftList.java
@@ -0,0 +1,12 @@
+package by.epam.learn.Airport;
+
+import java.util.ArrayList;
+
+public class AircraftList {
+    
+    ArrayList<AircraftThread> aircrafts;
+
+    public AircraftList(ArrayList<AircraftThread> aircrafts) {
+        this.aircrafts = aircrafts;
+    }
+}
