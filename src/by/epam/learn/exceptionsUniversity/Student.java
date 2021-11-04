7cd7377eb5e90fc6bf42f4572a7a2c0f5624a359
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-11-04 12:32:34 +0300

    Updated class Student - 2021/04/11

--- src/by/epam/learn/exceptionsUniversity/Student.java
+++ src/by/epam/learn/exceptionsUniversity/Student.java
@@ -6,16 +6,16 @@ public class Student {
     int id;
     String name;
     Faculty faculties;
-    Group groups;
-    Discipline disciplines;
+    Group group;
+    Discipline discipline;
     int mark;
 
-    public Student(int id, String name, Faculty faculties, Group groups, Discipline disciplines, int mark) {
+    public Student(int id, String name, Faculty faculties, Group group, Discipline discipline, int mark) {
         this.id = id;
         this.name = name;
         this.faculties = faculties;
-        this.groups = groups;
-        this.disciplines = disciplines;
+        this.group = group;
+        this.discipline = discipline;
         this.mark = mark;
     }
 
@@ -26,19 +26,60 @@ public class Student {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(java.lang.String name) {
+        this.name = name;
     }
 
     public Faculty getFaculties() {
         return faculties;
     }
 
-    public Group getGroup() {
-        return groups;
+    public void setFaculty(Faculty faculty) {
+        this.faculties = faculty;
     }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group groups) {
+        this.group = groups;
+    }
+
+    public Discipline getDiscipline() {
+        return discipline;
+    }
+
+    public void setDiscipline(Discipline discipline) {
+        this.discipline = discipline;
+    }
+
     public int getMark() {
         return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", faculties=" + faculties +
+                ", groups=" + group +
+                ", disciplines=" + discipline +
+                ", mark=" + mark +
+                '}';
     }
 
     @Override
@@ -46,8 +87,10 @@ public class Student {
         if (this == o) return true;
         if (!(o instanceof Student)) return false;
         Student student = (Student) o;
-        return getId() == student.getId() && Objects.equals(getName(), student.getName())
-                && Objects.equals(getFaculties(), student.getFaculties()) && Objects.equals(getGroup(), student.getGroup());
+        return getId() == student.getId() && getMark() == student.getMark()
+                && Objects.equals(getName(), student.getName()) && Objects.equals(getFaculties(),
+                student.getFaculties()) && Objects.equals(getGroup(), student.getGroup())
+                && Objects.equals(getDiscipline(), student.getDiscipline());
     }
 
     @Override
