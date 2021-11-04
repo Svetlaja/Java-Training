af44420e03524a7482951c3cfd7e8ee0e9c956bb
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-11-04 13:10:24 +0300

    Updated  MarkValueException - deleted parameter (int markValue)

--- src/by/epam/learn/UniversityExceptions/MarkValueException.java
+++ src/by/epam/learn/UniversityExceptions/MarkValueException.java
@@ -1,13 +1,8 @@
 package by.epam.learn.UniversityExceptions;
 
 public class MarkValueException extends Exception {
-    int mark;
-
-    public int getMark() {
-        return mark;
-    }
-    public MarkValueException(String markMessage, int markValue) {
-        super(markMessage);
-        mark = markValue;
+   
+    public MarkValueException(String markMessage) {
+        super(markMessage);      
     }
 }
