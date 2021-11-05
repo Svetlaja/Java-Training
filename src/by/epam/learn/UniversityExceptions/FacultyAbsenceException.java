fe6f5ffb6daf6d71e4a975145bd080a209c28bb8
Sviatlana Hodzina <72805375+SvetlanaHodzina@users.noreply.github.com>
2021-11-05 13:46:39 +0300

    Renamed message in  FacultyAbsenceException - 2021/05/11

--- src/by/epam/learn/UniversityExceptions/FacultyAbsenceException.java
+++ src/by/epam/learn/UniversityExceptions/FacultyAbsenceException.java
@@ -2,7 +2,7 @@ package by.epam.learn.UniversityExceptions;
 
 public class FacultyAbsenceException extends Exception {
 
-    public FacultyAbsenceException(String disciplineMessage) {
-        super(disciplineMessage);
+    public FacultyAbsenceException(String facultyMessage) {
+        super(facultyMessage);
     }
 }
